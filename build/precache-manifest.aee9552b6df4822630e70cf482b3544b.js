f486c98fc7a0cc9102592a034dd01f34f89e76a8
Bikki Mahato <42492682+bikkimahato@users.noreply.github.com>
2019-10-01 16:21:21 +0530

    deployed

---
+++ build/precache-manifest.aee9552b6df4822630e70cf482b3544b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f50e12e7b0f77f3f0378f93b5f7f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d50bd5de1744c4cb180",
+    "url": "/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "98298205bff9629344f5",
+    "url": "/static/js/2.bd792e6a.chunk.js"
+  },
+  {
+    "revision": "1d50bd5de1744c4cb180",
+    "url": "/static/js/main.626c6e47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/UI-Routers/Routes/main.js
+++ src/UI-Routers/Routes/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import ScreenInterface from '../../UI-Pages/ScreenInterface'
 
 class Routes extends React.Component {
